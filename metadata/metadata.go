6736ae355f935b33944a265ea94705c708668e4d
aarayamb <aarayamb@ucsd.edu>
2018-06-07 16:37:47 -0700

    changes to zookeeper interface

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,10 +1,21 @@
 package metadata
 
 import (
-	"go-graph/storage"
 	"github.com/google/uuid"
 )
 
+// Metadata : Interface exposing zookeeper functionality
 type Metadata interface {
-	GetNodeLocation(nodeId uuid.UUID) storage.Storage
+	//creates a Znode for a graph vertex
+	createVertexZnode(graphID uuid.UUID, vertexID uuid.UUID, partitionID uuid.UUID) error
+	//creates a Znode for a graph edge
+	createEdgeZnode(graphID uuid.UUID, vertexID uuid.UUID, partitionID uuid.UUID) error
+	//Method to create a Znode
+	getVertexLocation(graphID uuid.UUID, vertexID uuid.UUID) (error, []string)
+	//Method to create a Znode
+	setVertexLocation(graphID uuid.UUID, vertexID uuid.UUID, partitionID uuid.UUID) error
+	//Method to create a Znode
+	getEdgeLocation(graphID uuid.UUID, edgeID uuid.UUID) (error, []string)
+	//Method to create a Znode
+	setEdgeLocation(graphID uuid.UUID, edgeID uuid.UUID, partitionID uuid.UUID) error
 }
