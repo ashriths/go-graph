cd6379b246ab5d1df71c0c417031083eb12c968c
Ashrith Sheshan <ashrith.sheshan@gmail.com>
2018-06-09 21:10:19 -0700

    Clean up index on removal

--- storage/index.go
+++ storage/index.go
@@ -41,11 +41,12 @@ func (self *Index) CreateVertexIndex(vertex *graph.Vertex) error {
 
 	self.VertexIndex[vertex.GetUUID().String()] = list.New()
 	system.Logln(vertex.Json())
-	_, ok := self.GraphVertexIndex[vertex.GetGraphId().String()]
-	if !ok {
-		self.GraphVertexIndex[vertex.GetGraphId().String()] = list.New()
-	}
-	self.GraphVertexIndex[vertex.GetGraphId().String()].PushBack(vertex.GetUUID().String())
+
+	//_, ok := self.GraphVertexIndex[vertex.GetGraphId().String()]
+	//if !ok {
+	//	self.GraphVertexIndex[vertex.GetGraphId().String()] = list.New()
+	//}
+	//self.GraphVertexIndex[vertex.GetGraphId().String()].PushBack(vertex.GetUUID().String())
 
 	return nil
 }
@@ -57,6 +58,11 @@ func (self *Index) CreateEdgeIndex(edge *graph.Edge) error {
 	key := edge.GetUUID().String() + "::" + SRC_PREFIX
 	self.EdgeIndex[key] = edge.SrcVertex.String()
 
+	self.VertexIndex[edge.SrcVertex.String()].PushBack(EdgeIndex{
+		Name: edge.Name,
+		Id:   edge.GetUUID().String(),
+	})
+
 	key = edge.GetUUID().String() + "::" + DEST_PREFIX
 	self.EdgeIndex[key] = edge.DestVertex.String()
 	return nil
@@ -66,32 +72,28 @@ func (self *Index) RemoveVertexIndex(vertexId uuid.UUID) error {
 	self.indexLock.Lock()
 	defer self.indexLock.Unlock()
 
-	//outEdges := self.VertexIndex[vertexId.String()]
+	outEdges := self.VertexIndex[vertexId.String()]
 	delete(self.VertexIndex, vertexId.String())
 
-	// Remove from EdgeIndex
-	//i := outEdges.Front()
-	//for i != nil {
-	//
-	//
-	//	i = i.Next()
-	//}
+	//Remove from EdgeIndex
+	i := outEdges.Front()
+	for i != nil {
+		self.protectedRemoveEdgeIndex(i.Value.(EdgeIndex).Id)
+		i = i.Next()
+	}
 	return nil
 }
 
-//func (self *Index) RemoveEdgeIndex(edgeId uuid.UUID) error {
-//	self.indexLock.Lock()
-//	defer self.indexLock.Unlock()
-//
-//	//outEdges := self.VertexIndex[vertexId.String()]
-//	delete(self.VertexIndex, edgeId.String())
-//
-//	//// Remove from EdgeIndex
-//	//i := outEdges.Front()
-//	//for i != nil {
-//	//
-//	//
-//	//	i = i.Next()
-//	//}
-//	return nil
-//}
+// This is not synchronized. Call this only if you hold the lock to index
+func (self *Index) protectedRemoveEdgeIndex(edgeId string) {
+	delete(self.EdgeIndex, edgeId+"::"+DEST_PREFIX)
+	delete(self.EdgeIndex, edgeId+"::"+SRC_PREFIX)
+}
+
+func (self *Index) RemoveEdgeIndex(edgeId uuid.UUID) error {
+	self.indexLock.Lock()
+	defer self.indexLock.Unlock()
+
+	self.protectedRemoveEdgeIndex(edgeId.String())
+	return nil
+}
